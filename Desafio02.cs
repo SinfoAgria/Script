f715022dd615108bcd9b3cd30b89c2e63be48d98
SinfoAgria <68078099+SinfoAgria@users.noreply.github.com>
2020-07-10 00:49:36 -0500

    Add files via upload

---
+++ Desafio02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor de t= ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor de a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            double c = (180 - a - 90);
+            Console.WriteLine("El valor de c es= " + c);
+
+            double rad = a * (Math.PI / 180);
+            double y = (Math.Sin(rad) * t);
+            Console.WriteLine("El valor de y es= " + y);
+
+            double casiZ = (t * t) - (y * y);
+            double z = Math.Sqrt(casiZ);
+            Console.WriteLine("El valor de z es= " + z);
+        }
+    }
+}
