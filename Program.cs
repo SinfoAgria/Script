56f80ef2ce6ea6cc9f18bfde4a982ae665ceb0c9
SinfoAgria <68078099+SinfoAgria@users.noreply.github.com>
2020-07-09 23:36:19 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,39 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Desafio01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Entrada del usuario de la variable X1
-            Console.Write("Ingrese X1: ");
-            double x1 = double.Parse(Console.ReadLine());
+            int b = 90;
 
-            // Entrada del usuario de la variable Y1
-            Console.Write("Ingrese Y1: ");
-            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y: ");
+            double y = double.Parse(Console.ReadLine());
 
-            // Entrada del usuario de las variables restantes
-            // ...
-            Console.Write("Ingrese X2: ");
-            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese z: ");
+            double z = double.Parse(Console.ReadLine());
 
-            Console.Write("Ingrese Y2: ");
-            double y2 = double.Parse(Console.ReadLine());
+            double t = (Math.Sqrt((y * y) + (z * z)));
 
-            double m = ((y2 - y1) / (x2 - x1));
+            Console.WriteLine("Resultado de t= " + t);
 
-            Console.WriteLine("Resultado de la pendiente= " + m);
+            double sinC = (z / t);
+            double c = Math.Asin(sinC);
+            double cGrados = c * (180.0 / Math.PI);
+            Console.WriteLine("Ángulo c= " + cGrados);
 
-            double b = (y1 - (m * x1));
+            double a = (180 - cGrados - b);
+            Console.WriteLine("Angulo a= " + a);
 
-            Console.WriteLine("Resultado del intercepto= " + b);
-
-            double d = (Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-
-            Console.WriteLine("Resultado de la distancia= " + d);
+            Console.WriteLine("Ángulo b= " + b);
         }
     }
 }
