c21e8c1ff9f289a493a791ced79f0d555b18e86b
SinfoAgria <68078099+SinfoAgria@users.noreply.github.com>
2020-07-12 00:29:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte un dato en binario: ");
+            int b0 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte un dato en binario: ");
+            int b1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte un dato en binario: ");
+            int b2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte un dato en binario: ");
+            int b3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte un dato en binario: ");
+            int b4 = int.Parse(Console.ReadLine());
+
+            double resultado = (b0 * 16) + (b1 * 8) + (b2 * 4) + (b3 * 2) + (b4 * 1);
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
