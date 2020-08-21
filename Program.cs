5d62d557f9173708b6f3b02123ce431e2bf37b97
SinfoAgria <68078099+SinfoAgria@users.noreply.github.com>
2020-08-20 21:11:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, n = 100;
+            double sinx = 0, x = 0, d = 0;
+
+            Console.WriteLine("Por favor, ingrese el número: ");
+            x = double.Parse(Console.ReadLine());
+
+            for (i = 0; i <= n; i++)
+            {
+                d = (2 * i) + 1;
+                double resultado = Factorial(d);
+
+                sinx += (Math.Pow(-1, i) / resultado) * (Math.Pow(x, d));
+                Console.WriteLine("Sin(x)= " + sinx);
+            }
+        }
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+    }    
+}
